4267a9c1372fe4a8f3dd2229e10ff42365b1e867
alex <hortinstein@gmail.com>
2019-02-13 00:48:59 -0500

    finished day 2

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,14 @@ use std::error::Error;
 
 use aoc_day1;
 use aoc_day2;
+use aoc_day3;
 use common::Config;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     match config.day {
         1 => aoc_day1::run(config).unwrap(),
         2 => aoc_day2::run(config).unwrap(),
+        3 => aoc_day2::run(config).unwrap(),
         _ => print!("day not yet supported"),
     }
 
--- src/lib/day_libs/aoc_day3/src/lib.rs
+++ src/lib/day_libs/aoc_day3/src/lib.rs
@@ -1,7 +1,36 @@
+use common::Config;
+use std::collections::HashMap;
+//use std::io::{self, Read, Write};
+
+type Result<T> = ::std::result::Result<T, Box<::std::error::Error>>;
+
+fn part1(input: &str) -> Result<i32> {
+    Ok(check_sum)
+}
+
+use difference::{Changeset, Difference};
+
+fn part2(input: &str) -> Result<i32> {
+    Ok(1)
+}
+
+pub fn run(config: Config) -> Result<()> {
+    println!("Day3:");
+    part1(&(config.contents))?;
+    part2(&(config.contents))?;
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn part1() {
+        assert_eq!(4, 2 + 2);
     }
+    #[test]
+    fn part2() {
+        assert_eq!(4, 2 + 2);
+    }
+
 }
